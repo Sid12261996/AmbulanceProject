dcf9cc053fade52a6bde9751e07fbab61737269f
Tarun <tarunkethwalia18@gmail.com>
2018-11-04 23:13:59 +0530

    Add files via upload

---
+++ First.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<title>Ambulance</title>
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  	<link href="CSS/Front.css" rel="stylesheet" />
+</head>
+<body id="grad1">
+
+<!--Logo-->
+<div class="container-fluid col-md-12">
+<div class="cards text-center">
+  <img class="card-img-top" src="Img/Plus.png">
+</div>
+</div>
+
+<!--Form-->
+<div class="container-fluid col-md-12 text-center padding">
+  <form action="" method="">
+    <table align="center" cellpadding="3px">
+      <tr>
+        <td>Phone No. / Email ID</td>
+        <td><input type="text" name="" /></td>
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="password" name="" /></td>
+      </tr>
+    </table>
+    <div class="signup">
+      <h6>New? Sign Up <a href="">Here</a></h6>
+      <button type="submit" class="btn-success curve">LogIn</button>
+    </div>
+  </form>
+  <h6>Or</h6>
+  <button type="submit" class="btn-danger curve">Emergency Call</button>
+</div>
+
+</body>
+</html>
